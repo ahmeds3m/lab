eb950f922c7e5b063d04e55b2827b01fd8db8cda
ahmeds3m <ahmedhassansheikh03@gmail.com>
2023-11-02 16:36:53 -0600

    finished

---
+++ main.py
@@ -0,0 +1,17 @@
+f = open("three_digit_numbers.txt")
+sample =f.read()
+cols = sample.split()
+# print(cols)
+num_list=[]
+for col in cols:
+    num = int(col)
+    num_list.append(num)
+# print(num_list)
+start = min(num_list)
+sorted_list = []
+for count in range(start, max(num_list)):
+   
+    if count not in num_list:
+        sorted_list.append(count)
+with open("sorted_numbers.txt",'w') as file:
+    file.write(str(sorted_list))
